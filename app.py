8c8e653dcfd645348f77f4ed7d7e1a4c0a8f1050
MarPls <35261805+MariosPliatsikas@users.noreply.github.com>
2023-12-21 18:25:23 +0100

    the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from itsdangerous import URLSafeTimedSerializer
 from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -13,12 +15,12 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'your-email-username'
 app.config['MAIL_PASSWORD'] = 'your-email-password'
-
+    
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 mail = Mail(app)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-
+        
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -26,7 +28,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(120))
     email_verified = db.Column(db.Boolean, default=False)
     role = db.Column(db.String(80), default='user')
-
+        
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -34,6 +36,22 @@ def load_user(user_id):
 @app.route("/")
 def index():
     return "Hello, World!"
+
+@app.route('/upload', methods=['POST'])
+@login_required
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part in the request.', 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file.', 400
+
+    filename = secure_filename(file.filename)
+    file.save(os.path.join('/path/to/save', filename))
+
+    return 'File uploaded successfully.', 200
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -45,13 +63,12 @@ def register():
         user = User(username=username, email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-
         token = s.dumps(email, salt='email-confirm-salt')
         msg = Message('Confirm Email', sender='noreply@yourdomain.com', recipients=[email])
         link = url_for('confirm_email', token=token, _external=True)
         msg.body = 'Follow this link to confirm your email: {}'.format(link)
         mail.send(msg)
-
+            
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -65,8 +82,8 @@ def confirm_email(token):
     user.email_verified = True
     db.session.add(user)
     db.session.commit()
-    return redirect(url_for('login'))
-
+    return redirect(url_for('login'))  
+        
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -76,14 +93,14 @@ def login():
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('home'))
-    return render_template('login.html')
-
+    return render_template('login.html') 
+    
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
+    
 @app.route('/home')
 @login_required
 def home():
